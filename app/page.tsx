9563d4cf20e06f594469c85eae27bf4ebf50ebd1
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-08-29 22:07:05 -0300

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,56 @@
-import NextLink from "next/link";
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
+import NextImage from "next/image";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { Input } from "@nextui-org/input";
+import { Image } from "@nextui-org/image";
+import { Button } from "@nextui-org/button";
+import { User } from "@nextui-org/user";
+import { Card, CardFooter } from "@nextui-org/card";
+const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "AIzaSyAq9L4mRG9lPlDrplm2iHBgTg1HAQamiDU";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className={title()}>Make&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+        <br />
+        <h1 className={title()}>websites regardless of your design experience.</h1>
+        <h2 className={subtitle({ class: "mt-4" })}>Beautiful, fast and modern React UI library.</h2>
+      </div>
+      <div className="flex justify-start container mx-auto max-w-4xl">
+        <User
+          name="Nome do motorista"
+          description="Motorista particular"
+          avatarProps={{
+            src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+          }}
+        />
+      </div>
+      <br />
+      <br />
+      <br />
+      <div className="flex flex-col md:flex-row items-center justify-center gap-4 container mx-auto max-w-4xl">
+        <Input isRequired type="text" label="ponto atual" radius="sm" className="md:w-96" />
+        <Input isRequired type="text" label="destino" radius="sm" />
+      </div>
 
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					as={NextLink}
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					as={NextLink}
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
+      <Card isFooterBlurred className="w-full flex items-center max-w-4xl md:p-10 py-4 pb-12">
+        <Image
+          as={NextImage}
+          width={1400}
+          height={600}
+          className="w-full"
+          src={`https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=13&size=700x300&key=${API_KEY}`}
+          alt="Taxi"
+        />
+        <CardFooter className="absolute flex justify-center bottom-0 z-10 border-t-1 dark:border-default-100 border-default-60 dark:bg-black/10 bg-white/10">
+          <Button radius="sm" size="sm" color="success" className="w-80 font-bold text-slate-100 text-sm">
+            começar
+          </Button>
+        </CardFooter>
+      </Card>
 
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+      {/* <Card className="w-full flex items-center max-w-4xl md:p-10 py-4"></Card> */}
+    </section>
+  );
 }
